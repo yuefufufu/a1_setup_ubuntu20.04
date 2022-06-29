e9df76a8a7ba05f40e28d915177c97501f68bbb5
“yuefufufu” <yue.fufufu@gmail.com>
2022-06-29 20:45:28 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # a1_setup_ubuntu20.04
+
+echo "# a1_setup_ubuntu20.04" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yuefufufu/a1_setup_ubuntu20.04.git
+git push -u origin main
---
+++ a1_setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+mv lcm ~/
+mv unitree_legged_sdk ~/
+mv gazebo_ros_pkgs ~/catkin_ws/src/
+
+cd
+sudo 
+sudo apt remove ros-noetic-gazebo*
+sudo apt remove libgazebo*
+sudo apt remove gazebo*
+sudo apt install gazebo9*
+
+cd ~/catkin_ws/src
+rosdep update
+rosdep check --from-paths . --ignore-src --rosdistro noetic
+rosdep install --from-paths . --ignore-src --rosdistro noetic -y
+
+cd ~/catkin_ws/
+catkin build
+
+source /opt/ros/noetic/setup.bash
+source ~/catkin_ws/devel/setup.bash
+
+cd ~/lcm
+mkdir build
+cd build
+cmake ../
+make
+sudo make install
+cd ~/unitree_legged_sdk
+mkdir build
+cd build
+cmake ../
+make
+
+cd ~/a1_setup
+mv unitree_ros ~/catkin_ws/src/
+mv unitree_ros_to_real ~/catkin_ws/src/
+
+cd
+echo "source /usr/share/gazebo-9/setup.sh" >> ~/.bashrc
+echo "export ROS_PACKAGE_PATH=~/catkin_ws:${ROS_PACKAGE_PATH}" >> ~/.bashrc
+echo "export GAZEBO_PLUGIN_PATH=~/catkin_ws/devel/lib:${GAZEBO_PLUGIN_PATH}" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=~/catkin_ws/devel/lib:${LD_LIBRARY_PATH}" >> ~/.bashrc
+echo "# 3_1, 3_2" >> ~/.bashrc
+echo "export UNITREE_SDK_VERSION=3_2" >> ~/.bashrc
+echo "export UNITREE_LEGGED_SDK_PATH=~/unitree_legged_sdk" >> ~/.bashrc
+echo "#export ALIENGO_SDK_PATH=~/aliengo_sdk" >> ~/.bashrc
+echo "# amd64, arm32, arm64" >> ~/.bashrc
+echo "export UNITREE_PLATFORM="amd64"" >> ~/.bashrc
+echo "export LCM_PATH=/usr/local/lib/liblcm.so.1" >> ~/.bashrc
+
+cd ~/catkin_ws
+catkin build unitree_legged_msgs
+catkin build 
+
