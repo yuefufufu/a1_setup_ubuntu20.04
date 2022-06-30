9f1e9e14500e94bd97f360e59930d5794ca1d21a
“yuefufufu” <yue.fufufu@gmail.com>
2022-06-30 18:55:25 +0900

    commit

--- noetic_setup.sh
+++ noetic_setup.sh
@@ -2,7 +2,7 @@
 
 cd 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl # if you haven't already installed curl
+sudo apt install curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install ros-noetic-desktop-full
@@ -10,6 +10,8 @@ echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 sudo apt install python3-rosdep
+sudo apt install python3-osrf-pycommon python3-catkin-tools
+sudo apt upgrade
 sudo rosdep init
 rosdep update
 mkdir -p ~/catkin_ws/src
