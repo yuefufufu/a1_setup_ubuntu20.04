a357413365e1c227aa5bf3ae45c578e8e85afc77
yuefufufu <104189406+yuefufufu@users.noreply.github.com>
2022-07-25 19:06:28 +0900

    Update a1_setup_first.sh

--- a1_setup_first.sh
+++ a1_setup_first.sh
@@ -34,6 +34,7 @@ echo 'export UNITREE_PLATFORM="amd64"' >> ~/.bashrc
 echo 'export LCM_PATH=/usr/local/lib/liblcm.so.1' >> ~/.bashrc
 
 sudo apt-get install -y ros-noetic-controller-interface ros-noetic-joint-state-controller ros-noetic-effort-controllers ros-noetic-joint-trajectory-controller
+sudo apt-get install ros-noetic-teleop-twist-keyboard
 sudo apt install -y net-tools
 sudo apt install -y ipmiutil
 
